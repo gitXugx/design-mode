57d4d5d32ba1cb5ac14c93a04ddce1a9efa06a3e
gitXugx <603949699@qq.com>
2018-12-14 11:14:28 +0800

    fist commit

---
+++ src/main/java/Application.java
@@ -0,0 +1,11 @@
+/**
+ * @author ：ex-xugaoxiang001
+ * @description ：
+ * @copyright ：	Copyright 2018 yowits Corporation. All rights reserved.
+ * @create ：2018/12/14 11:12
+ */
+public class Application {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/test/java/TestApplication.java
@@ -0,0 +1,8 @@
+/**
+ * @author ：ex-xugaoxiang001
+ * @description ：
+ * @copyright ：	Copyright 2018 yowits Corporation. All rights reserved.
+ * @create ：2018/12/14 11:13
+ */
+public class TestApplication {
+}
